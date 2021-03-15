06a3ca3fb8267e476a2e0aa200fa3dbf228f9c11
Viktoria Kapinus <solvd@solvds-Mac-mini.local>
2021-03-15 17:16:42 +0300

    added asserts

--- cypress/integration/examples/test.js
+++ cypress/integration/examples/test.js
@@ -1,13 +1,20 @@
-let logo ='h1.logotype'
+// <reference types='cypress' />
+let logo ='div.logotype'
 let searchWord = 'Утюг'
 let searchFrame = '[id^=j-search]'
 let searchButton = 'button[name="sa"]'
+let resultHeader = 'div.b-content > h1'
 let itemName = 'span.result__name'
-describe("Search", function() {
-    it("Search word", function() {
+let titles = new Array();
+
+
+describe('SearchTest', () => {
+    it('Search word', () => {
         cy.visit("https://www.21vek.by/")
+        cy.get(logo).should('be.visible')
         cy.get(searchFrame).type(searchWord)
         cy.get(searchButton).click()
+        cy.get(resultHeader).should('be.visible')
         cy.get(itemName).should('contain', searchWord)
     })
 })
